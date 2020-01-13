6dad900e4d126c6e03bfa8a6e2203cb20d2ba550
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2020-01-13 13:57:15 +0100

    modif taille nav

--- inc/header.php
+++ inc/header.php
@@ -12,22 +12,23 @@
 <body>
 
 <header id="header">
-    <div class="wrap"></div>
-    <div class="logo">
-        <img src="https://via.placeholder.com/100x50.png?text=DCRYPT" alt="">
-    </div>
-    <nav class="nav">
-        <ul>
-            <li><a href="#">Redirection</a></li>
-            <li><a href="#">Redirection</a></li>
-            <li><a href="#">Redirection</a></li>
-            <li><a href="#">Redirection</a></li>
-            <li><a href="../trame/trame.php">Lecture trame</a></li>
-        </ul>
-        <div class="connected">
-            <a class="connect"href="#">Connexion</a>
-            <a class="burger" href="#"><i></i></a>
+    <div class="wrap">
+        <div class="logo">
+            <img src="https://via.placeholder.com/100x50.png?text=DCRYPT" alt="">
         </div>
-    </nav>
+        <nav class="nav">
+            <ul>
+                <li><a href="#">Redirection</a></li>
+                <li><a href="#">Redirection</a></li>
+                <li><a href="#">Redirection</a></li>
+                <li><a href="#">Redirection</a></li>
+                <li><a href="../trame/trame.php">Lecture trame</a></li>
+            </ul>
+            <div class="connected">
+                <a class="connect" href="#">Connexion</a>
+                <a class="burger" href="#"><i></i></a>
+            </div>
+        </nav>
+    </div>
     <!--    <div class="clear"></div>-->
 </header>
