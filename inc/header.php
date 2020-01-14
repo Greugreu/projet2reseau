cba2d885868e80e671b7cef58581c12e49fcd0ae
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2020-01-14 09:58:02 +0100

    header fixed

--- inc/header.php
+++ inc/header.php
@@ -11,12 +11,12 @@
 
 <body>
 
+<div class="wrap">
 <header id="header">
-    <div class="wrap">
-        <div class="logo">
-            <a href="index.php"><img src="assets/img/logo.png" alt=""></a>
-        </div>
-        <nav class="nav">
+        <nav id="nav" class="nav" class="sticky">
+            <div class="logo">
+                <a href="index.php"><img src="assets/img/logo.png" alt=""></a>
+            </div>
             <ul>
                 <li><a href="#">Redirection</a></li>
                 <li><a href="#">Redirection</a></li>
@@ -28,36 +28,28 @@
                 <a class="connect" href="login.php">Connexion</a>
                 <nav role='navigation'>
                     <div id="menuToggle">
-                        <input type="checkbox" />
+                        <input type="checkbox"/>
                         <span></span>
                         <span></span>
                         <span></span>
                         <ul id="menu">
-                            <a href="#"><li>Home</li></a>
-                            <a href="#"><li>About</li></a>
-                            <a href="#"><li>Info</li></a>
-                            <a href="#"><li>Contact</li></a>
+                            <a href="#">
+                                <li>Home</li>
+                            </a>
+                            <a href="#">
+                                <li>About</li>
+                            </a>
+                            <a href="#">
+                                <li>Info</li>
+                            </a>
+                            <a href="#">
+                                <li>Contact</li>
+                            </a>
                         </ul>
                     </div>
                 </nav>
             </div>
         </nav>
-    </div>
-<div class="clear"></div>
+    <div class="clear"></div>
 </header>
-
-<section id="section">
-
-    <div class="wrap2">
-
-        <h2 id="h2">dcrypt</h2>
-        <div class="boxleft">
-            <p><a class="button btnl" href="">Lisez votre trame</a></p>
-        </div>
-        <div class="boxright">
-            <p><a class="button btnr" href="">Inscrivez-vous</a></p>
-        </div>
-        <div class="clear"></div>
-    </div>
-
-</section>
+</div>
--- index.php
+++ index.php
@@ -9,6 +9,21 @@
 require_once ("inc/header.php"); ?>
 
 
+    <section id="section">
+
+        <div class="wrap2">
+
+            <h2 id="h2">dcrypt</h2>
+            <div class="boxleft">
+                <p><a class="button btnl" href="">Lisez votre trame</a></p>
+            </div>
+            <div class="boxright">
+                <p><a class="button btnr" href="">Inscrivez-vous</a></p>
+            </div>
+            <div class="clear"></div>
+        </div>
+
+    </section>
 
 
 
