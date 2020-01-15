426ad13006aa13ebc656550d08a52b72303a2192
Lecuyer Stéphane <47177118+lecuyste@users.noreply.github.com>
2020-01-15 16:59:23 +0100

    Je fais joujou avec les trames

--- trame.php
+++ trame.php
@@ -1,6 +1,6 @@
 <?php
 
-$fichier = file_get_contents('capture.json');
+$fichier = file_get_contents('capture_axel.json');
 
 $json = json_decode($fichier, true);
 //    echo "<pre>";
@@ -9,6 +9,7 @@ $json = json_decode($fichier, true);
 
 //$ipAddrSrc = $json[0]['_source']['layers']['ip']['ip.src'];
 //echo "<br>" . $ipAddrSrc;
+
 
 require_once("inc/header.php"); ?>
 
@@ -30,11 +31,12 @@ require_once("inc/header.php"); ?>
         $nb = count($json);
         $udp = 0;
         $tcp = 0;
+        $tabIp = array();
 
         for ($i = 0; $i < $nb; $i++) {
             echo '<tr>';
             $row = $json[$i]['_source']['layers'];
-            if  (isset($row['frame'])){
+            if (isset($row['frame'])) {
                 echo '<td>' . $json[$i]['_source']['layers']['frame']['frame.time'] . '</td>';
             } else {
                 echo '<td></td>';
@@ -42,6 +44,61 @@ require_once("inc/header.php"); ?>
             if (isset($row['ip'])) {
                 echo '<td>' . $json[$i]['_source']['layers']['ip']['ip.src'] . '</td>';
                 echo '<td>' . $json[$i]['_source']['layers']['ip']['ip.dst'] . '</td>';
+                array_push($tabIp, $json[$i]['_source']['layers']['ip']['ip.dst']);
+
+                foreach ($tabIp as $ipDest) {
+                    $curl = curl_init();
+
+                    curl_setopt_array($curl, array(
+                        CURLOPT_URL => "https://freegeoip.app/json/" . $ipDest,
+                        CURLOPT_RETURNTRANSFER => true,
+                        CURLOPT_ENCODING => "",
+                        CURLOPT_MAXREDIRS => 10,
+                        CURLOPT_TIMEOUT => 120,
+                        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                        CURLOPT_CUSTOMREQUEST => "GET",
+                        CURLOPT_HTTPHEADER => array(
+                            "accept: application/json",
+                            "content-type: application/json"
+                        ),
+                    ));
+
+                    $response = curl_exec($curl);
+                    $err = curl_error($curl);
+
+                    curl_close($curl);
+
+                    if ($err) {
+                        echo "cURL Error #:" . $err;
+                    } else {
+                        //echo $response;
+                        $test = json_decode($response);;
+                        $countryName = $test->country_name;
+                        //debug($longitude);
+                        //debug($latitude);
+                        //var_dump($test);
+                        //var_dump($test->ip);
+                        //echo $response
+                        $z = [
+                            'country_name' => $countryName
+                        ];
+                        if (empty($z['country_name'])) {
+                            unset($z['country_name']);
+                        } else {
+                            //$countryName = $z['country_name'];
+//                            echo "<pre>";
+//                            var_dump($z);
+//                            echo "</pre>";
+                            //echo $z['country_name'][0];
+                            $tab[] = $z;
+
+                            //$result = array_merge($z);
+                            echo "<pre>";
+                            var_dump($tab);
+                            echo "</pre>";
+                        }
+                    }
+                }
             } else {
                 echo '<td></td>';
                 echo '<td></td>';
@@ -53,21 +110,21 @@ require_once("inc/header.php"); ?>
                 echo '<td></td>';
                 echo '<td></td>';
             }
-        if (isset($row['udp'])) {
+            if (isset($row['udp'])) {
                 echo '<td>UDP</td>';
                 echo '<td>' . $json[$i]['_source']['layers']['udp']['udp.srcport'] . '</td>';
                 echo '<td>' . $json[$i]['_source']['layers']['udp']['udp.dstport'] . '</td>';
                 $udp++;
-            }else if (isset($row['tcp'])) {
+            } else if (isset($row['tcp'])) {
                 echo '<td>TCP</td>';
                 echo '<td>' . $json[$i]['_source']['layers']['tcp']['tcp.srcport'] . '</td>';
                 echo '<td>' . $json[$i]['_source']['layers']['tcp']['tcp.dstport'] . '</td>';
                 $tcp++;
             } else {
-            echo '<td></td>';
-            echo '<td></td>';
-            echo '<td></td>';
-        }
+                echo '<td></td>';
+                echo '<td></td>';
+                echo '<td></td>';
+            }
 
             echo '</tr>';
         }
@@ -75,12 +132,16 @@ require_once("inc/header.php"); ?>
         ?>
         </tbody>
     </table>
+    <!-- Stat a faire :
+    Nb de connexion à la minute en splittant frame.time
+    Voir pour géoloc les @ip
+    Voir pour reconnaitre Netflix, Fb, etc...-->
     <script>
         console.log(<?=$tcp?>)
         var ctx = document.getElementById('myChart').getContext('2d');
         var chart = new Chart(ctx, {
             // The type of chart we want to create
-            type: 'pie',
+            type: 'doughnut',
 
             // The data for our dataset
             data: {
@@ -96,7 +157,6 @@ require_once("inc/header.php"); ?>
                     data: [<?=$tcp?>, <?=$udp;?>]
                 }]
             },
-
             // Configuration options go here
             options: {}
         });
