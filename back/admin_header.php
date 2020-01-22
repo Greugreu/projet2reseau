3b2ded6ef1c81e56e0da87d1b1bbf268baf65ad2
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2020-01-22 10:12:06 +0100

    changement admin title

--- back/admin_header.php
+++ back/admin_header.php
@@ -7,7 +7,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<title>Administration ADN</title>
+<title>Administration DCRYPT</title>
 
 <!-- Custom fonts for this template-->
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
